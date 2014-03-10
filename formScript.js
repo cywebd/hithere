f6cb83244b53b68c1ff001d6c34b67231022c1c7
bannanaleaf <bannanaleaf@gmail.com>
2014-03-10 11:30:47 +0000

    Defined text variable

--- formScript.js
+++ formScript.js
@@ -2,6 +2,7 @@
    "use strict";
     var form = document.getElementById("theForm");
     var textarea = form.textyarea;
+    var text = form.texty;
     addEventListener("submit", function(event) {
        event.preventDefault();
        if ( textarea.value === "" ) {
